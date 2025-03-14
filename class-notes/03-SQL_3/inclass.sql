7776a90451e83f839a54ba935b593bf52b037efe
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-03-14 23:24:26 +0300

    Add SQL examples for LEFT JOIN, aggregate functions, and data manipulation

--- class-notes/03-SQL_3/inclass.sql
+++ class-notes/03-SQL_3/inclass.sql
@@ -1,3 +1,5 @@
+-- Active: 1741976409637@@127.0.0.1@3306
+---------------------------------- JOINS ----------------------------------
 
 -- * INNER JOIN -- Yalnızca kesişen kayıtları getirir.
 -- * (Alternatif Yazım: JOIN) Default JOIN yöntemi INNER JOIN'dir. (Piyasa kullanımı: INNER JOIN)
@@ -57,3 +59,56 @@ JOIN "Album" t2 USING ("ArtistId");
 
 
 --? Bütün sanatçıları göster. Hangi sanatçı hangi albüme sahip onu da göster. Ama albüm sahibi olmayan kayıtlara NULL yaz.
+SELECT t1."ArtistId", t1."Name", t2."Title", t2."AlbumId"
+FROM "Artist" t1
+LEFT JOIN "Album" t2 USING ("ArtistId");
+
+/* FUNCTIONS (count & IN) */
+
+-- **Albüm tablosundaki toplam kayıt sayısını getir**
+SELECT * FROM "Album";
+
+SELECT count(*) 'total-album-number' FROM "Album";
+SELECT count("AlbumId") 'total-album-number' FROM "Album";
+
+-- **Her ülkenin toplam fatura sayısını getir**
+SELECT "BillingCountry", count("InvoiceId") 'total-invoices'
+FROM "Invoice"
+GROUP BY "BillingCountry";
+
+-- **Bir ülkede birden fazla müşteri olan ülkeleri getir**
+SELECT Country , count("Country") 'country-count'
+FROM Customer
+GROUP BY "Country"
+HAVING count("Country") > 1;
+
+-- **ABD ve Kanada'daki faturaları listele**
+SELECT * 
+FROM "Invoice"
+WHERE BillingCountry IN ('USA', 'Canada');
+
+
+/* INSERT & UPDATE & DELETE */
+
+SELECT * FROM genre;
+
+-- **Yeni bir müzik türü (Genre) ekle**
+INSERT INTO Genre (GenreId, Name) VALUES ( 27, 'Halk Muzigi');
+
+-- **Türk Halk Müziği ve Pop Müziği türlerini ekle**
+INSERT INTO Genre (GenreId, Name) 
+VALUES 
+    ( 28, 'Türk Halk Müziği'), 
+    ( 29, 'Türk Pop Müziği'); 
+
+-- **exp-1 olarak bir müzik türünün adını güncelle**
+UPDATE "Genre"
+SET NAME = 'Alman Rap'
+WHERE "GenreId" = 26;
+
+-- **Genre tablosundaki tüm verileri sil**
+DELETE FROM genre;
+
+DELETE FROM "Invoice"
+WHERE "InvoiceId" = 1;
+
