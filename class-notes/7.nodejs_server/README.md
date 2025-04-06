c7beed6cc45a676a6b4c9fec871f722f268ac42d
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-04-06 04:00:56 +0300

    Add server setup and routing in index.js; create README.md for project instructions

---
+++ class-notes/7.nodejs_server/README.md
@@ -0,0 +1,4 @@
+npm init -y
+npm i nodemon
+nodemon
+npm installl dotenv
--- class-notes/7.nodejs_server/index.js
+++ class-notes/7.nodejs_server/index.js
@@ -1,1 +1,19 @@
 console.log("server");
+
+require("dotenv").config() //.env dosyasını kullanabilmek için dotenv modülünü dahil ettik.
+
+const PORT = process.env?.PORT || 8000 //PORT değişkenini .env dosyasından alıyoruz. Eğer yoksa 3000 portunu kullanıyoruz.
+const HOST= process.env?.HOST  || "127.0.0.1"
+
+console.log(PORT, HOST);
+const http= require("http") //http modülünü dahil et, node.js modülleri, node kurulumu ile gelen modüller. 
+const app= http.createServer((req, res)=>{
+ console.log("server is running");
+})
+app.listen(PORT, ()=>console.log(`server running: http://${HOST}:${PORT}`))
+if(req.url=="/"){
+    res.end(" <h1>Welcome FS18 </h1>")
+}else if(req.url=="/blogs"){
+    res.end("<h1>Blogs</h1>")
+
+}
