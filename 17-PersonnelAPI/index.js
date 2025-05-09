374ac1d5024d928d6d752e0b8b83d3472e93f525
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-05-09 07:32:33 +0300

    Initialize PersonnelAPI project with essential dependencies and setup

---
+++ 17-PersonnelAPI/index.js
@@ -0,0 +1,39 @@
+"use strict";
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+//&Middlewares
+app.use(express.json());
+require("express-async-errors");
+//*Session-Cookies
+const session = require("cookie-session");
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+  })
+);
+//!Query Handler
+app.use(require(""));
+
+require("");
+//Routes
+app.all("", (req, res) => {
+  res.send({
+    message: "Welcome to Personnel Api",
+  });
+});
+app.use("/departments", require(""));
+app.use("/personnel", require(""));
+app.use("*", (req, res) => {
+  res.status(404).send({
+    error: true,
+    message: "This route can not found",
+  });
+});
+//Todo Error Handler
+app.use(require(""))
+//Run Server
+app.listen(PORT, ()=>console.log("Running: http://127.0.0.1:"+PORT))
+//?Syncronization
+require("")
---
+++ 17-PersonnelAPI/readme.md
@@ -0,0 +1,3 @@
+* npm i express dotenv mongoose express-async-errors
+* npm i cookie-session
+* npm i jsonwebtoken
