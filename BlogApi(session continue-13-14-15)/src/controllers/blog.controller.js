7e460c0b40a66d244bf7fcc7cc866f9a4b035d02
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-05-09 05:29:25 +0300

    feat: Implement find, search, sort, and pagination middleware for blog posts

--- BlogApi(session continue-13-14-15)/src/controllers/blog.controller.js
+++ BlogApi(session continue-13-14-15)/src/controllers/blog.controller.js
@@ -71,51 +71,7 @@ module.exports.blogCategory = {
 //Todo BlogPost Controller
 module.exports.blogPost = {
   list: async (req, res) => {
-    //* FILTERING
-    //Filter: absolute equality
-    //Search: partial equality
-    console.log(req.query);
-    // {
-    //   published: '1',
-    //   categoryId: '6819439d6dcb2cacfac67063',
-    //   title: 'test 5',
-    //   sort: 'asc'
-    // } clg ye gelen yanıt.
-
-    //*nested query:   filter: { published: '1', categoryId: '6819439d6dcb2cacfac67063' }
-
-    const filter = req.query?.filter || {};
-    console.log(filter);
-
-    //SEARCHING
-    const search = req.query?.search || {};
-    console.log(search.title);
-    //console.log(search["title"]);
-    //{ "<field>": { "$regex": "pattern", "$options": "<options>"
-    //.populate("categoryId");  //!POPULATE() categoryId yi okumak için.
-    //search["title"]= "this is new value"
-
-    // for(let key in search) console.log(search[key]);
-    for (let key in search)
-      search[key] = { $regex: search[key], $options: "i" };
-    console.log(search);
-
-    //SORTING
-    const sort = req.query?.sort || {};
-    //PAGINATION
-    //LIMIT
-    let limit = parseInt(req.query?.limit);
-    limit = limit > 0 ? limit : parseInt(process.env.PAGE_SIZE) || 20; //.env den gelen veriler string old için parseInt yap. Number() da NaN verebilir 25a ya mesela.
-    console.log(typeof limit); //string
-    //PAGE
-    let page = parseInt(req.query?.page);
-    page = page > 0 ? page : 1;
-    //SKIP
-    let skip = parseInt(req.query?.skip);
-    skip = skip > 0 ? skip : (page-1)*limit
-    console.log(page, limit, skip);
-
-    const result = await BlogPost.find({ ...filter, ...search }).sort(sort).skip(skip).limit(limit)
+  const result= await res.getModelList(BlogPost)
 
     res.status(200).send({
       error: false, //success:true da olur.
