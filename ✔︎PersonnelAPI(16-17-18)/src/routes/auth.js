51f12bbee0b29fc8684611503119e950afa4b4df
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-05-15 23:05:54 +0300

    feat: Add authentication routes for login and logout

---
+++ ✔︎PersonnelAPI(16-17-18)/src/routes/auth.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const router = require('express').Router()
+
+const { login, logout } = require('../controllers/auth')
+
+// URL : /auth
+
+router.post('/login', login)
+
+router.all('/logout', logout)
+
+
+module.exports = router
