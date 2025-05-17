f850c7358e7fa6c6be3aa45400530cce09d6a5c9
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-05-17 15:22:22 +0300

    feat: update login functionality to ensure token creation for active users and clean up code

--- ✔︎PersonnelAPI(16-17-18)/src/controllers/auth.js
+++ ✔︎PersonnelAPI(16-17-18)/src/controllers/auth.js
@@ -1,7 +1,7 @@
 "use strict";
 const passwordEncrypt = require("../helpers/passwordEncrypt");
 const Personnel = require("../models/personnel");
-const Token= require("../models/token")
+const Token = require("../models/token");
 module.exports = {
   login: async (req, res) => {
     const { username, email, password } = req.body;
@@ -13,26 +13,23 @@ module.exports = {
       //* if(user.password== passwordEncrypt(password)) yerine direkt yukarda password yaz. o zaten personnel modelde set edilmişti.
       if (user) {
         if (user.isActive) {
-
-        //?TOKEN 
-        const tokenData= await Token.create({
-          userId: user._id,
-          token: passwordEncrypt(user._id + Date.now())      //! id yi encrypt yaptık. Tokeni benzersiz oluşturma için. third part packeage ile de yapılır.
-        })
-
-          res.status(200).send({
-            error: false,
-            token: tokenData.token,
-            user
-          })
+          //?TOKEN
+          let tokenData = await Token.findOne({ userId: user._id });
+          if (!tokenData) {
+            tokenData = await Token.create({
+              userId: user._id,
+              token: passwordEncrypt(user._id + Date.now()), //! id yi encrypt yaptık. Tokeni benzersiz oluşturma için. third part package ile de yapılır.
+            });
+          } 
+            res.status(200).send({
+              error: false,
+              token: tokenData.token,
+              user,
+            })
         } else {
           res.errorStatusCode = 401;
-          throw new Error("User is not active.")
+          throw new Error("User is not active.");
         }
-        res.status(200).send({
-          error: false,
-          message: "OK",
-        });
       } else {
         res.errorStatusCode = 401;
         throw new Error("Wrong username or email");
