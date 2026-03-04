4e355a1f97dc4956f2213b382220d475b7d8af2a
Bahar <ylmzbhr1@gmail.com>
2026-03-03 21:58:17 -0500

    Refactor code structure and optimize performance across multiple modules

---
+++ 06-nodejs_intro/index.js
@@ -0,0 +1,41 @@
+"use strict"; //Bu satır, JavaScript'in "Strict Mode" (Katı Mod) özelliğini etkinleştirir. Strict Mode, JavaScript kodunu daha güvenli ve hataya karşı daha duyarlı hale getirir. Bu modda, bazı hatalar daha erken tespit edilir ve bazı kötü uygulamalar engellenir.
+const express = require("express"); //Bu satır, Express.js kütüphanesini projene dahil eder. Express hızlı ve minimal bir Node.js web framework'üdür.  Express modülünü yükler ve express fonksiyonunu alır.
+const app = express(); //Bu satır, Express uygulaması oluşturur. Artık bu app objesi, web sunucusu oluşturmak ve çeşitli işlemler yapmak için kullanılacak.
+
+const port = 3000; //Port numarasını tanımlıyoruz. Bu port üzerinden sunucuya erişim sağlanacak.
+app.listen(port, () => {
+  //Sunucuyu belirtilen portta dinlemeye başlıyoruz. Sunucu çalışmaya başladığında konsola bir mesaj yazdırıyoruz.
+  console.log(`Server is running on port ${port}`);
+});
+
+//! TEK BİR FONKSİYON İÇİN KULLANILDI
+// const testFunct= require("./module/module")
+// testFunct()
+
+//! ÇOKLU FONKSİYON İÇİN KULLANILDI
+// const {testing, imrenbirth, baharbirth, imren}= require("./module/module")
+// imrenbirth()
+// baharbirth()
+// testing()
+// console.log(imren);
+
+
+require("./module/module");
+const {
+  testFunct,
+  birthday1,
+  birthday2,
+  imren,
+  numbers: [oddNumber, evenNumber],
+} = require("./module/module");
+
+testFunct();
+birthday1();
+birthday2();
+console.log(imren);
+oddNumber();
+evenNumber();
+
+console.log("nodemon yüklendi");
+
+console.log("again, checked");
---
+++ 06-nodejs_intro/module/module.js
@@ -0,0 +1,42 @@
+"use strict";
+//JavaScript’te Strict Mode (Katı Mod)’u aktif eder. Yani JS’i daha kurallı, güvenli ve hataya karşı hassas hale getirir.
+
+//! TEK BİR FONKSİYON İÇİN KULLANILDI
+
+const testFunct = () => {
+  console.log("this is a function");
+};
+ //module.exports = testFunct;
+
+//! ÇOKLU FONKSİYON İÇİN KULLANILDI
+    const imren = "14.01";
+const birthday1 = function () {
+
+  console.log(`imren is born on ${imren}`);
+};
+
+    const bahar = "14.02";
+const birthday2 = function () {
+
+  console.log(`bahar is born on ${bahar}`);
+};
+
+ module.exports = {
+   testing: testFunct,
+   imrenbirth: birthday1,
+   baharbirth: birthday2,
+   imren
+ };
+
+
+const evenNumber= function(){
+    console.log("2,4,6,8,10,12,14,16,18");
+}
+const oddNumber= function(){
+    console.log("1,3,5,7,9,11,13,15,17,19");
+}
+
+module.exports = { testFunct, birthday1, birthday2, imren, numbers:[evenNumber, oddNumber] };
+
+
+console.log("checked");
